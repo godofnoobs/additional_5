6bb9311a67964b761302d0eb576449862c19d9c6
godofnoobs <n_ska@bk.ru>
2018-02-21 02:49:29 +0300

    i'm asleep

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+'use strict'
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+//function check(str, bracketsConfig) {
+    var left = []
+    var right = []
+    var stack = [];
+    
+    for (var i in bracketsConfig) {
+        left.push(bracketsConfig[i][0]);
+        right.push(bracketsConfig[i][1]);
+    }
+
+    for (var i = 0; i < str.length; i++) {
+
+        if (left.indexOf(str[i]) != -1) {
+            if ((right.indexOf(str[i]) != -1) && (stack.length%2 != 0)) {
+                stack.pop();
+                continue;
+            }
+            stack.push(str[i]);
+        }
+        else if (right.indexOf(str[i]) != -1) {
+            var opened = right.indexOf(str[i]);
+            if ((stack[stack.length-1] != left[opened]) || (stack.length == 0))
+                return false;
+            else stack.pop();
+        }
+    }
+    if (stack.length)
+        return false;
+    return true;
 }
+
+//var config = [['(', ')']];
+//alert(check('())(', config));
+
